b76332082ca3b6d35c91a067d919cd4bd344f77a
vwfox <markus.hochstein@software-hochstein.de>
2024-10-13 11:55:20 +0200

    initial commit fix license headers add docker build
    add license file, reformate error pages
    
    fix line intents in error html files
    
    Signed-off-by: vwfox <markus.hochstein@software-hochstein.de>
    
    modified ci for build and docker using cache
    
    Signed-off-by: vwfox <markus.hochstein@software-hochstein.de>

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2024 Contributors to the  Eclipse Foundation.
+
+This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+
+  SPDX-License-Identifier: EPL-2.0
+
+Contributors:
+  Markus Hochstein - inital setup
+  Stefan Bischof - inital setup
+*/
+
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginPlaywright from 'eslint-plugin-playwright'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+
+  {
+    ...pluginPlaywright.configs['flat/recommended'],
+    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
+  skipFormatting,
+]
---
+++ eslint_report.md
@@ -0,0 +1,2 @@
+> **Found `0 errors, 0 warnings` - Generated on Sun, 13 Oct 2024 09:40:10 GMT.**
+---
