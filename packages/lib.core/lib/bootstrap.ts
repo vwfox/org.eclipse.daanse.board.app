b38a67a99463a01e5d8253654245b40c3640edae
vwfox <markus.hochstein@software-hochstein.de>
2025-03-21 17:31:05 +0100

    wip lerna and vite

---
+++ packages/lib.core/lib/bootstrap.ts
@@ -0,0 +1,38 @@
+/*********************************************************************
+ * Copyright (c) YYYY Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Name (Company) - description
+ **********************************************************************/
+
+
+import {container} from "lib.core";
+import {RootService} from "./RootService";
+
+const boot = async (listOfPackages:string[])=> {
+
+
+    for (const pack of listOfPackages){
+        try{
+          /* @vite-ignore */
+          let {init} = await import(pack);
+          await init(container);
+
+        }catch (e){
+            console.warn(`error on init Package ${pack}`);
+            console.warn(e);
+        }
+    }
+    container.bind<RootService>(RootService).toSelf().inSingletonScope();
+    const rs = container.get<RootService>(RootService);
+    rs.activate();
+
+
+}
+export {boot}
