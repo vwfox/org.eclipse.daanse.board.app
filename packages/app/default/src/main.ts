15c6a7714542299c8ca536f4b4ba6cf06f5b0c90
ssemenkoff <ssemenkoff@gmail.com>
2025-03-23 13:26:10 +0100

    Licence

--- packages/app/default/src/main.ts
+++ packages/app/default/src/main.ts
@@ -1,10 +1,21 @@
+/**
+Copyright (c) 2023 Contributors to the  Eclipse Foundation.
+This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+SPDX-License-Identifier: EPL-2.0
+
+Contributors: Smart City Jena
+ */
+
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 import { register } from "org.eclipse.daanse.board.app.lib.module1";
 import { container } from "org.eclipse.daanse.board.app.lib.core";
@@ -13,7 +24,7 @@ register(container);
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia())
+// app.use(router)
 
 app.mount('#app')
