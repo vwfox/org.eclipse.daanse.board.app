026446076c01ab8125121ca84d8b87d1d59b783a
ssemenkoff <ssemenkoff@gmail.com>
2025-04-02 03:00:57 +0200

    Restore graphql datasource

--- packages/lib/datasource/graphql/src/classes/index.ts
+++ packages/lib/datasource/graphql/src/classes/index.ts
@@ -13,107 +13,126 @@
 
 // import { extractDataByPath } from "@/utils/helpers";
 
-import { BaseDatasource, IBaseConnectionConfiguration } from 'org.eclipse.daanse.board.app.lib.datasource.base'
+import { injectable, inject, Container } from 'inversify'
+import {
+  BaseDatasource,
+  IBaseConnectionConfiguration,
+} from 'org.eclipse.daanse.board.app.lib.datasource.base'
+import { identifiers } from 'org.eclipse.daanse.board.app.lib.core'
+import {
+  identifier,
+  ConnectionRepository,
+} from 'org.eclipse.daanse.board.app.lib.repository.connection'
 // import type { ComputedString } from "@/plugins/variables/ComputedString";
 
-
-export interface IGraphQLStoreConfiguration extends IBaseConnectionConfiguration {
-  connection: string;
-  query: string;
-  variables?: any;
+export interface IGraphQLStoreConfiguration
+  extends IBaseConnectionConfiguration {
+  connection: string
+  query: string
+  variables?: any
 }
 
 export class GraphQLStore extends BaseDatasource {
-  private connection: any;
-  private query: string;
+  private connection: any
+  private query: string
 
-  constructor(configuration: IGraphQLStoreConfiguration) {
-    super(configuration);
+  constructor(
+    configuration: IGraphQLStoreConfiguration,
+    @inject(identifiers.CONTAINER) private container: Container,
+  ) {
+    super(configuration)
 
-    this.connection = configuration.connection;
-    this.query = configuration.query;
-    this.pollingInterval = configuration.pollingInterval ?? 5000;
+    this.connection = configuration.connection
+    this.query = configuration.query
+    this.pollingInterval = configuration.pollingInterval ?? 5000
     if (this.pollingEnabled) {
-        this.startPolling(this.pollingInterval);
+      this.startPolling(this.pollingInterval)
     }
   }
 
   get fetcher() {
-    // const connectionRepository = this.connectionRepository;
-    // if (!connectionRepository) {
-    //   throw new Error('ConnectionRepository is not provided to Store Classes');
-    // }
-    // const connection = connectionRepository.getConnection(this.connection) as GraphQLConnection;
-
-    // return connection.fetcher;
-    return {}
+    const connectionRepository = this.container.get(
+      identifier,
+    ) as ConnectionRepository
+    if (!connectionRepository) {
+      throw new Error('ConnectionRepository is not provided to Store Classes')
+    }
+    const connection = connectionRepository.getConnection(
+      this.connection,
+    ) as any
+    return connection.fetcher
   }
 
   async getData(type: string): Promise<any> {
-    // if (this.query) {
-    //   const request = await this.fetcher({ query: this.query }) as any;
-    //   const result = (await request.next()).value.data;
-    //   return result as unknown as DataMap[T];
-    // } else {
-    //   console.warn('Query is not provided for GraphQLStore');
-    //   return null as unknown as DataMap[T];
-    // }
+    if (this.query) {
+      const request = (await this.fetcher({ query: this.query })) as any
+      const result = (await request.next()).value.data
+      return result
+    } else {
+      console.warn('Query is not provided for GraphQLStore')
+      return null
+    }
   }
 
   async getOriginalData() {
-    throw new Error("Not Implemented");
+    throw new Error('Not Implemented')
   }
 
   parseToDataTable(data: any) {
-    // if (!Array.isArray(data)) return { items: [], headers: [], rows: [] };
+    if (!Array.isArray(data)) return { items: [], headers: [], rows: [] }
 
-    // const headers: string[] = ['index'];
-    // const rows: any[] = [];
+    const headers: string[] = ['index']
+    const rows: any[] = []
 
-    // const items = data.map((item: any, index: number) => {
-    //   if (typeof item !== 'object') return {};
+    const items = data.map((item: any, index: number) => {
+      if (typeof item !== 'object') return {}
 
-    //   const row: IDataTableRow = {
-    //     index
-    //   };
+      // const row: IDataTableRow = {
+      const row: any = {
+        index,
+      }
 
-    //   for (const key in item) {
-    //     if (typeof item[key] === 'object' || Array.isArray(item[key])) continue;
+      for (const key in item) {
+        if (typeof item[key] === 'object' || Array.isArray(item[key])) continue
 
-    //     if (!headers.includes(key)) {
-    //       headers.push(key);
-    //     }
+        if (!headers.includes(key)) {
+          headers.push(key)
+        }
 
-    //     row[key] = item[key];
-    //   }
+        row[key] = item[key]
+      }
 
-    //   return row;
-    // });
+      return row
+    })
 
     // items.forEach((item: IDataTableRow, index:number) => {
-    //   rows[index] = [];
+    items.forEach((item: any, index: number) => {
+      rows[index] = []
 
-    //   headers.forEach((header: string) => {
-    //     rows[index].push(item[header]);
-    //   })
-    // })
+      headers.forEach((header: string) => {
+        rows[index].push(item[header])
+      })
+    })
 
-    // return { items, headers, rows };
+    return { items, headers, rows }
   }
 
   callEvent(event: string, params: any) {
-    console.warn(`Event "${event}" is not available for this type of store`, params)
-  };
+    console.warn(
+      `Event "${event}" is not available for this type of store`,
+      params,
+    )
+  }
 
   destroy(): void {
-    this.stopPolling();
+    this.stopPolling()
   }
 
   static validateConfiguration(configuration: IGraphQLStoreConfiguration) {
     if (!configuration.connection) {
-      return false;
+      return false
     }
 
-    return true;
+    return true
   }
 }
