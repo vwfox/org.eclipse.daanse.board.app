3ce043cbe545d4c2aa0bb60599f2f892bff7c101
ssemenkoff <ssemenkoff@gmail.com>
2025-04-02 02:40:16 +0200

    Restore csv datasource

--- packages/lib/datasource/csv/src/classes/index.ts
+++ packages/lib/datasource/csv/src/classes/index.ts
@@ -12,11 +12,17 @@
  **********************************************************************/
 
 // import { extractDataByPath } from "@/utils/helpers";
+import { injectable, inject, Container } from 'inversify'
 import {
   BaseDatasource,
-  IBaseConnectionConfiguration,
+  type IBaseConnectionConfiguration,
 } from 'org.eclipse.daanse.board.app.lib.datasource.base'
-// import type { ComputedString } from "@/plugins/variables/ComputedString";
+import { identifiers } from 'org.eclipse.daanse.board.app.lib.core'
+import {
+  identifier,
+  type IConnection,
+  ConnectionRepository,
+} from 'org.eclipse.daanse.board.app.lib.repository.connection'
 
 export interface ICsvStoreConfiguration extends IBaseConnectionConfiguration {
   resourceUrl: string
@@ -30,11 +36,15 @@ export interface ICsvParseResult {
   mappedRows: any[]
 }
 
+@injectable()
 export class CsvStore extends BaseDatasource {
   private connection: any
   private resourceUrl: string
 
-  constructor(configuration: ICsvStoreConfiguration) {
+  constructor(
+    configuration: ICsvStoreConfiguration,
+    @inject(identifiers.CONTAINER) private container: Container,
+  ) {
     super(configuration)
 
     this.connection = configuration.connection
@@ -48,38 +58,50 @@ export class CsvStore extends BaseDatasource {
   }
 
   async getOriginalData() {
-    // const connectionRepository = this.connectionRepository;
-    // if (!connectionRepository) {
-    //   throw new Error('ConnectionRepository is not provided to Store Classes');
-    // }
-    // const connection = connectionRepository.getConnection(this.connection) as CsvConnection;
-    // const req = await connection.fetch({ url: this.resourceUrl.value });
-    // if (!req.ok) return [];
-    // const text = await req.text();
+    const connectionRepository = this.container.get(
+      identifier,
+    ) as ConnectionRepository
+    if (!connectionRepository) {
+      throw new Error('ConnectionRepository is not provided to Store Classes')
+    }
+    const connection = connectionRepository.getConnection(
+      this.connection,
+    ) as IConnection
+    const req = await connection.fetch({ url: this.resourceUrl })
+    if (!req.ok) return []
+    const text = await req.text()
+
+    // TODO: fix after adding utils
     // const data = CSV.parse(text);
     // return data;
+    return text
   }
 
   async getData(type: string): Promise<any> {
-    // const connectionRepository = this.connectionRepository;;
-    // if (!connectionRepository) {
-    //   throw new Error('ConnectionRepository is not provided to Store Classes');
-    // }
-    // const connection = connectionRepository.getConnection(this.connection) as CsvConnection;
-    // const req = await connection.fetch({ url: this.resourceUrl.value });
-    // if (!req.ok) return null as unknown as DataMap[T];
-    // const text = await req.text();
+    const connectionRepository = this.container.get(
+      identifier,
+    ) as ConnectionRepository
+    if (!connectionRepository) {
+      throw new Error('ConnectionRepository is not provided to Store Classes')
+    }
+    const connection = connectionRepository.getConnection(
+      this.connection,
+    ) as IConnection
+    const req = await connection.fetch({ url: this.resourceUrl })
+    if (!req.ok) return null
+    const text = await req.text()
     // const data: ICsvParseResult = CSV.parse(text);
-    // if (type === "DataTable") {
-    //   return {
-    //     headers: data.header,
-    //     items: data.mappedRows,
-    //     rows: data.rows,
-    //   } as IDataTable as DataMap[T];
-    // } else {
-    //   console.warn("Invalid data type");
-    //   return null as unknown as DataMap[T];
-    // }
+    if (type === 'DataTable') {
+      // return {
+      //   headers: data.header,
+      //   items: data.mappedRows,
+      //   rows: data.rows,
+      // } as IDataTable as DataMap[T];
+      return text
+    } else {
+      console.warn('Invalid data type')
+      return null
+    }
   }
 
   callEvent(event: string, params: any) {
